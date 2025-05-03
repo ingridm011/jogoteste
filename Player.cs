01994fd7676305a03984d73fcd02f298dc37b0e3
Ingrid <miranda.ingrid011@gmail.com>
2025-05-03 14:46:40 -0300

    actualization player

--- Player.cs
+++ Player.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
+    public float speed; //velocidade do personagem
+    private Rigidbody2D rig; //manipular a física
+    private Vector2 direction; //direção do personagem
 
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        //pegas as direções x e y da movimentação
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    //utilizar para quando for mexer com a fisica
+    private void FixedUpdate()
+    {
+        //move o personagem 
+        rig.MovePosition(rig.position + direction * speed * Time.fixedDeltaTime);
+    }
 }
